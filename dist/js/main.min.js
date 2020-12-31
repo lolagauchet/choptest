c0ba16360d129faedb96e5b44af662d41b5b8b42
Lola <lola.gauchet@gmail.com>
2020-12-31 18:09:43 +0100

    news section, clean code

--- dist/js/main.min.js
+++ dist/js/main.min.js
@@ -35,4 +35,34 @@
         ]
     });
 
+
+    $(document).on("click", ".js_faq", function () {
+        $(this).siblings().removeAttr("open");
+    });
+
+    $('.section__news-item').each(function () {
+        console.log($(this).attr('data-tag'))
+    });
+
+    $(document).on("click", ".js_tag", function () {
+        var $tag = $(this).attr('data-tag');
+
+        $(this).addClass('active').siblings().removeClass('active');
+        $('.js_news').removeClass('active');
+        $('.js_news').each(function () {
+            $itemTag = $(this).attr('data-tag');
+            if ($tag === $itemTag || $tag === 'all') {
+                $(this).addClass('active')
+            }
+        });
+        // check the number of items showed
+        $activeNb = $('.js_news.active').length;
+        if ($activeNb == 0) {
+            $('.js_news_list').prepend("<span class='js-no-result'>No news matching this tag</span>")
+        } else {
+            $('.js-no-result').remove()
+        }
+
+    });
+
 })(jQuery);
